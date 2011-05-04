b129237bb70cc5fc60964f417f339e48e5422b79
Adam Quirk <bullemhead@gmail.com>
2011-05-04 16:50:41 -0400

    settings

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -65,8 +65,9 @@ class Contact < ActiveRecord::Base
   named_scope :created_by, lambda { |user| { :conditions => [ "user_id = ?", user.id ] } }
   named_scope :assigned_to, lambda { |user| { :conditions => ["assigned_to = ?", user.id ] } }
 
-  simple_column_search :first_name, :last_name, :phone, :addresses, :email,
+  simple_column_search :first_name, :last_name, :city, :email,
     :match => lambda { |column| column == :email ? :middle : :start },
+    :match => lambda { |column| column == :city ? :middle : :start },
     :escape => lambda { |query| query.gsub(/[^\w\s\-\.']/, "").strip }
   uses_user_permissions
   acts_as_commentable
