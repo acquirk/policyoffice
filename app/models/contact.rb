6d8e8a8b579456119ac19ee3a3c4998aa6e41c1d
Adam Quirk <bullemhead@gmail.com>
2011-05-07 13:09:54 -0400

    contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -144,7 +144,7 @@ class Contact < ActiveRecord::Base
       :assigned_to => params[:account][:assigned_to],
       :access      => params[:access]
     }
-    %w(first_name last_name title source email born_on alt_email phone mobile blog linkedin facebook twitter do_not_call background_info).each do |name|
+    %w(first_name last_name title source email alt_email phone mobile blog linkedin facebook twitter do_not_call background_info).each do |name|
       attributes[name] = model.send(name.intern)
     end
     
